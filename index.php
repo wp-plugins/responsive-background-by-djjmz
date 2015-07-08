7799a364ad4cb4358ca29abb1db3e87f4c17cb10
djjmz <djjmz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-08 14:32:35 +0000

    Small update
    git-svn-id: https://plugins.svn.wordpress.org/responsive-background-by-djjmz/trunk@1194806 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,87 +1,154 @@
 <?php
-	/*
-		Plugin Name: Responsive Background by DJJMZ
-		Plugin URI: 
-		Description: Easy way change background image/color to fully responsive image. Compatible with all browsers: computers, all phone and tablets.
-		Version: 1.1
-		Author: djjmz
-		Author URI: 
-	*/
-	add_action('wp_head', 'rb_custom_css');
-	function rb_custom_css() {
-    echo "<style type=\"text/css\">
-body {	
-background-image:url('".get_option('rb_background')."');	
+/*
+Plugin Name: Responsive Background by DJJMZ
+Plugin URI:
+Description: Easy way change background image/color to fully responsive image. Compatible with all browsers: computers, all phone and tablets.
+Version: 1.2
+Author: djjmz
+Author URI:
+*/
+add_action('wp_head', 'rb_custom_css') ;
+function rb_custom_css()
+{
+  echo "<style type=\"text/css\">
+body {
+background-image:url('" . get_option('rb_background') . "');
 background-repeat: no-repeat;
 background-position: center center;
 background-attachment: fixed;
-background-size: cover;	
+background-size: cover;
 }
-</style>\r\n";
+</style>\r\n" ;
 }
-	add_action('admin_menu', 'rb_add_menu');
-	function rb_add_menu() {
-		add_options_page('RB Settings', 'RB Settings', 'manage_options', 'rb_settings', 'rb_settings');
-	}
-	function rb_settings() {
-		if(isset($_POST['change']) and !empty($_POST['url'])){
-			if(checkRemoteFile($_POST['url'])){
-			$option_exists = (get_option('rb_background', null) !== null);			
-			if ($option_exists) {
-				update_option('rb_background', sanitize_text_field($_POST['url']));
-				} else {
-				add_option('rb_background', sanitize_text_field($_POST['url']));
-			}
-			}
-		}
-		echo '<div class="wrap">
-		<div class="metabox-holder has-center-sidebar"> 
-		<div id="post-body">
-		<div id="post-body-content">
-		<div class="postbox">
-		<div class="inside">
-		<div align="center">
-		<form method="post" action = "">
+add_action('admin_menu', 'rb_add_menu') ;
+function rb_add_menu()
+{
+  add_options_page('RB Settings', 'RB Settings', 'manage_options', 'rb_settings', 'rb_settings') ;
+}
+function rb_settings($current = 'external')
+{
+  $active_tab = isset ($_GET['tab']) ? $_GET['tab'] : 'external' ;
+  if ( isset ($_GET['tab']))
+    $active_tab = $_GET['tab'];
+  $message = '' ;
+  if ( isset ($_POST['add']))
+  {
+    if ( isset ($_POST['url']))
+    {
+      if ( rb_checkRemoteFile($_POST['url']))
+      {
+        $option_exists = ( get_option('rb_background', null) !== null ) ;
+        if ($option_exists)
+        {
+          update_option( 'rb_background', sanitize_text_field($_POST['url'])) ;
+        }
+        else
+        {
+          add_option( 'rb_background', sanitize_text_field($_POST['url'])) ;
+        }
+        $message = 'The background has been changed' ;
+      }
+      else
+      {
+        $message = 'Incorrect file format' ;
+      }
+    }
+  }
+  if ( isset ($_POST['upload']))
+  {
+    if ($_FILES['file']['name'] != '')
+    {
+      $file = $_FILES['file']['name'];
+      $upload_dir = wp_upload_dir() ;
+      $upload_path = $upload_dir['path'] . '/' ;
+      $allowed_filetypes = array('.jpg', '.png', '.gif', '.bmp') ;
+      $ext = substr( $file, strpos($file, '.'), strlen($file) - 1 ) ;
+      if ( !in_array($ext, $allowed_filetypes))
+      {
+        $message = 'Incorrect file format' ;
+      }
+      else
+      {
+        $file = str_replace(' ', '_', $file) ;
+        $file = strtolower($file) ;
+        if ( move_uploaded_file($_FILES['file']['tmp_name'], $upload_path . $file))
+        {
+          $message = 'The background has been changed' ;
+          $option_exists = ( get_option('rb_background', null) !== null ) ;
+          if ($option_exists)
+          {
+            update_option( 'rb_background', sanitize_text_field($upload_dir['url'] . '/' . $file)) ;
+          }
+          else
+          {
+            add_option( 'rb_background', sanitize_text_field($upload_dir['url'] . '/' . $file)) ;
+          }
+        }
+        else
+        {
+          $message = 'Upload Failed. Please Check Your File Type!' ;
+        }
+      }
+    }
+  }
+  $active_tab = isset ($_GET['tab']) ? $_GET['tab'] : 'external' ;
+  if ( isset ($_GET['tab']))
+    $active_tab = $_GET['tab'];
+  $tabs = array('external' => 'External image', 'upload' => 'Upload image') ;
+  echo '<h2>' ;
+  foreach ($tabs as $tab => $name)
+  {
+    $class = ($active_tab == $tab) ? ' nav-tab-active' : '' ;
+    echo '<a class="nav-tab' . $class . '" href="?page=rb_settings&tab=' . $tab . '">' . $name . '</a>' ;
+  }
+  echo '</h2><div style="background:#ffffff;text-align:center;margin-top: -15px;max-width:1250px;min-width:500px;padding-top: 25px;padding-bottom: 25px;">' ;
+  if ($message != NULL)
+    echo '<b>' . $message . '</b>' ;
+  if ($active_tab == 'external')
+  {
+    echo '<form method="post" action = "">
 		Background URL:<br>
-		<input name="url" type="text" value="'.get_option('rb_background').'" size="60"/><br>
-		<input type="submit" name="change" value="Change">
-		</form>
-		</div>
-		</div>
-		</div>
-		</div>
-		</div>
-		</div>
-		</div>';	
-	}	
-function checkRemoteFile($url)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_NOBODY, 1);
-    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$content = curl_exec($ch);
-    if($content!==FALSE)
-    {
-		if(is_image($url))
-        return true;
-	else
-		return false;
-    }
-    else
-    {
-        return false;
-    }
+        <input type="text" name="url" value="' . get_option('rb_background') . '" size="50"/><br>
+		<input type="submit" name="add" value="Add">
+		</form>' ;
+  }
+  elseif ($active_tab == 'upload')
+  {
+    echo '<form method="post" action = "" enctype="multipart/form-data">
+        Choose file:<br>
+        <input type="file" name="file" id="file"><br>
+		<input type="submit" name="upload" value="Upload">
+		</form>' ;
+  }
+  echo '</div>' ;
 }
-function is_image($path)
+function rb_checkRemoteFile($url)
 {
-    $a = getimagesize($path);
-    $image_type = $a[2];
-     
-    if(in_array($image_type , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP)))
-    {
-        return true;
-    }
-    return false;
+  $ch = curl_init() ;
+  curl_setopt($ch, CURLOPT_URL, $url) ;
+  curl_setopt($ch, CURLOPT_NOBODY, 1) ;
+  curl_setopt($ch, CURLOPT_FAILONERROR, 1) ;
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
+  $content = curl_exec($ch) ;
+  if ($content !== FALSE)
+  {
+    if ( rb_is_image($url))
+      return true ;
+    else
+      return false ;
+  }
+  else
+  {
+    return false ;
+  }
+}
+function rb_is_image($path)
+{
+  $a = getimagesize($path) ;
+  $image_type = $a[2];
+  if ( in_array( $image_type, array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_BMP)))
+  {
+    return true ;
+  }
+  return false ;
 }
--- readme.txt
+++ readme.txt
@@ -3,13 +3,15 @@ Contributors: djjmz
 Requires at least: 3.6
 Tags: responsive, full background, responsive background, full, background, option, options, widget, Post, plugin, admin, posts, sidebar, google, twitter, images, comments, page, shortcode, image
 Tested up to: 4.1 
-Stable tag: 1.0
+Stable tag: 1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WYDR4ADESD2KU&lc=LT&item_name=Donate&no_note=1&no_shipping=1&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 == Changelog ==
+= 1.2 =
+* Add upload function
 
 = 1.1 =
 * Small code update
